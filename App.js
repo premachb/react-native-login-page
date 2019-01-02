21f9da408c5ed671b05b431e6fe3b3efa5786465
Brien Premachandiran <premachb@gmail.com>
2019-01-02 16:24:48 -0500

    Misc formatting changes

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './LoginScreen';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import LoginScreen from "./LoginScreen";
 
 export default class App extends React.Component {
   render() {
-    return (
-        <LoginScreen />
-    );
+    return <LoginScreen />;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
