1bcf3037ac83afd92ad9cf9101d31e008ba8729d
Brien Premachandiran <premachb@gmail.com>
2019-01-02 16:24:35 -0500

    Turn off autocapitilization for both inputs on login. Formatting changes

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,38 +1,53 @@
-import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default class LoginScreen extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = {userNameText: "", passwordText: ""};
-    }
+  constructor(props) {
+    super(props);
+    this.state = { userNameText: "", passwordText: "" };
+  }
 
-    handleLoginPressed () {
-        console.log("time to login!");
-    }
+  handleLoginPressed() {
+    console.log("time to login!");
+  }
 
-    render () {
-        return (
-            <View style={styles.container}>
-                <TextInput onChangeText={(text) => this.setState({userNameText: text})} style={styles.textInput} placeholder="Username" autoCorrect={false} autocapitalize="none" value={this.state.userNameText} />
-                <TextInput onChangeText={(text) => this.setState({passwordText: text})} style={styles.textInput} placeholder="Password" autoCorrect={false} autocapitalize="none" value={this.state.passwordText} />
-                <Button title="Login" onPress={this.handleLoginPressed} />
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput
+          onChangeText={text => this.setState({ userNameText: text })}
+          style={styles.textInput}
+          placeholder="Username"
+          autoCorrect={false}
+          autoCapitalize="none"
+          value={this.state.userNameText}
+        />
+        <TextInput
+          onChangeText={text => this.setState({ passwordText: text })}
+          style={styles.textInput}
+          placeholder="Password"
+          autoCorrect={false}
+          autoCapitalize="none"
+          secureTextEntry={true}
+          value={this.state.passwordText}
+        />
+        <Button title="Login" onPress={this.handleLoginPressed} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    textInput: {
-        backgroundColor: '#d1d1d1',
-        width: '100%',
-        marginBottom: 10,
-        padding: 20,
-    }
+  container: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  textInput: {
+    backgroundColor: "#d1d1d1",
+    width: "100%",
+    marginBottom: 10,
+    padding: 20
+  }
 });
